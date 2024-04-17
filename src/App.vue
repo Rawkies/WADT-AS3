b3529535bc27cbbcfc988caf56baafa6fb13fbd1
hasyir <22FTT1502@student.pb.edu.bn>
2024-04-17 14:34:59 +0800

    Refactor App.vue to remove unused logo styles, add profile image, and update description

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,10 @@ import profileImage from './assets/profile.jpeg';
 <template>
   <main class="py-6 px-4 sm:p-6 md:py-10 md:px-8">
     <h1 class="mt-1 text-lg font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white">PROFILE PAGE </h1>
-    <div class="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
-      <div class="center">
         <img :src="profileImage" alt="" class="w-full h-full object-cover rounded-lg sm:h-100 sm:col-span-2 lg:col-span-full" loading="lazy">
-      </div>
       <p class="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
         This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.
       </p>
-    </div>
   </main>
 </template>
 
